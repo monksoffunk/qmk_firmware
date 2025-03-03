e38199dd8a60e852ba2ceabbf47876151b96d1d7
Monksoffunk <monksoffunk@users.noreply.github.com>
2025-03-03 19:34:12 +0900

    Change via keymap
    - MT LCtrl, Tab for Tab key
    - MT LCtrl, Esc for LCtrl key

--- keyboards/25keys/aleth42/keymaps/via/keymap.c
+++ keyboards/25keys/aleth42/keymaps/via/keymap.c
@@ -36,18 +36,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * | Esc|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
      * |-----------------------------------------------------------|
-     * | Tab |  A |  S |  D |  F |  G |  H |  J |  K |  L | Ent    |
+     * | Tab*|  A |  S |  D |  F |  G |  H |  J |  K |  L | Ent    |
      * |-----------------------------------------------------------|
      * | LSft  |  Z |  X |  C |  V |  B |  N |  M |  , |  . |fn2(/)|
      * |-----------------------------------------------------------|
-     * | LCtl | LAlt| LGui|  SPC fn0  |   BS fn1    |RGui|RAlt|RCtl|
+     * | Esc* | LAlt| LGui|  SPC fn0  |   BS fn1    |RGui|RAlt|RCtl|
      * `-----------------------------------------------------------'
  */
     [_QWERTY] = LAYOUT(
         KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,
-        KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,
+        MT(MOD_LCTL, KC_TAB),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,
         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  LT(_ADJUST, KC_SLSH),
-        KC_LCTL, KC_LALT   , KC_LGUI, LT(_LOWER, KC_SPC),   LT(_RAISE, KC_BSPC), KC_RGUI, KC_RALT, KC_RCTL
+        MT(MOD_LCTL, KC_ESC), KC_LALT   , KC_LGUI, LT(_LOWER, KC_SPC),   LT(_RAISE, KC_BSPC), KC_RGUI, KC_RALT, KC_RCTL
         ),
 
  /* Lower Layer fn0
